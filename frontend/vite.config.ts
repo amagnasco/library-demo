fbc97dbdab4422c1c6ab52f24a1fe52408a61a33
Alessandro G. Magnasco <amagnasco@gradcenter.cuny.edu>
2026-03-24 12:22:21 -0500

    frontend + vite online

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,27 +4,17 @@ import path from "path"
 // https://vitejs.dev/config/
 
 export default defineConfig({
-    plugins: [react({
-        babel: {
-            plugins: [
-                ["babel-plugin-styled-components", {
-                    displayName: true,
-                    fileName: true
-                }]
-            ]
-        }
-    })],
     resolve: {alias: {"@src": path.resolve(__dirname, "./src/"),},},
     server: {
         watch: {
             usePolling: true,
         },
-        host: true,
-        //strictPort: true,
-        //port: 5173,
+        host: '0.0.0.0',
+        strictPort: true,
+        port: 5173,
         //allowedHosts: ['example.com']
     },
-    //root: 'src',
+    root: 'src',
     build: {
         outDir: './dist',
     emptyOutDir: true,
